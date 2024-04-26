5f1ec76c203b130a6a130e3e644d36603566f261
syymm <shiyuming233@gmail.com>
2024-04-26 17:29:50 +0900

    Initial commit

---
+++ src/main/java/com/example/aigleam/AigleamApplication.java
@@ -0,0 +1,13 @@
+package com.example.aigleam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigleamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AigleamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aigleam/AigleamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aigleam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AigleamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
